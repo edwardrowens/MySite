acd811fadb1f4fd6819906bb87942729b106a5dc
Eddie Owens <edward.owens@gamblitgaming.com>
2017-05-02 18:44:26 -0700

    Adding ability to highlight components

--- src/frontend/component/PageLinkComponent.js
+++ src/frontend/component/PageLinkComponent.js
@@ -2,22 +2,20 @@ var React = require('react')
 var Scroll = require('react-scroll')
 
 var PageLinkComponent = React.createClass({
-    getInitialState: function () {
-        return { displayText: false }
+    getInitialState: function() {
+        return ({
+            selected: false
+        })
     },
 
-    onMouseOver: function () {
-        this.setState({ displayText: true })
-    },
-
-    onMouseLeave: function () {
-        this.setState({ displayText: false })
+    onClick: function() {
+        this.setState({selected: true})
     },
 
     render: function (props) {
         return (
             <Scroll.Link to={this.props.link} smooth={true} duration={500}>
-                <h2 id="pageLinkComponent" onMouseLeave={this.onMouseLeave} onMouseOver={this.onMouseOver}>{this.state.displayText ? this.props.text : this.props.previewText}</h2>
+                <h2 className={this.props.className} style={{background: selected ? '#f2e6ff' : '#ffffff'}}>{this.props.text}</h2>
             </Scroll.Link>
         )
     }
--- src/frontend/container/PageLinksContainer.js
+++ src/frontend/container/PageLinksContainer.js
@@ -3,18 +3,20 @@ var React = require('react')
 var PageLinkComponent = require('../component/PageLinkComponent')
 var VerticalLineComponent = require('../component/VerticalLineComponent')
 
-function PageLinksContainer(props) {
-    return (
-        <div id="pageLinks">
-            <ul>
-                <li><PageLinkComponent previewText="A" link="contentContainer" text="About Me"/></li>
-                <li><PageLinkComponent previewText="Ex" link="workexperience" text="Experience"/></li>
-                <li><PageLinkComponent previewText="Ed" link="education" text="Education"/></li>
-                <li><PageLinkComponent previewText="S" link="skills" text="Skills"/></li>
-                <li><PageLinkComponent previewText="C" link="contact" text="Contact"/></li>
-            </ul>
-        </div>
-    )
-}
+var PageLinksContainer = React.createClass({
+    render: function (props) {
+        return (
+            <div id="pageLinks">
+                <ul>
+                    <li><PageLinkComponent link="contentContainer" text="About Me" /></li>
+                    <li><PageLinkComponent link="workexperience" text="Experience" /></li>
+                    <li><PageLinkComponent link="education" text="Education" /></li>
+                    <li><PageLinkComponent link="skills" text="Skills" /></li>
+                    <li><PageLinkComponent link="contact" text="Contact" /></li>
+                </ul>
+            </div>
+        )
+    }
+})
 
 module.exports = PageLinksContainer
