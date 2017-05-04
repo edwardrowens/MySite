2684aa3b4d1a4bc1610471cca8229f66bf902737
edwardrowens <eddiebeme@gmail.com>
2017-05-04 00:10:41 -0700

    checking why the app is so slow on heroku

--- src/frontend/component/AboutMeComponent.js
+++ src/frontend/component/AboutMeComponent.js
@@ -3,7 +3,7 @@ var ResourceLoader = require('./../services/ResourceLoader')
 var marked = require('marked')
 
 function AboutMeComponent(props) {
-    return <div dangerouslySetInnerHTML={{__html:marked(ResourceLoader('resources/documents/home.txt'))}}></div>
+    return <div dangerouslySetInnerHTML={{__html:marked('Intro will be written shortly')}}></div>
 }
 
 module.exports = AboutMeComponent
