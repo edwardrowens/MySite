f7385a4bfc9ca8c4901497767341e9bec18c3b8f
Eddie Owens <edward.owens@gamblitgaming.com>
2017-05-26 10:42:58 -0700

    No longer statically serving all of my source code even though it's publicly available here...

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,20 +1,16 @@
-var path = require('path');
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var favicon = require('serve-favicon');
+var path = require('path')
+var express = require("express")
+var bodyParser = require("body-parser")
+var app = express()
+var favicon = require('serve-favicon')
 
 // grab the configuration
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8000
 
-app.use(express.static(path.join(__dirname, '..')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(favicon(path.join(__dirname, '../../lib', 'resources', 'assets', 'favicon.png')));
+app.use(express.static(path.join(__dirname, '..', 'build')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(favicon(path.join(__dirname, '..', 'build', 'resources', 'assets', 'favicon.png')))
 
-app.get('/', function (req, res) {
-	res.sendFile(path.resolve(__dirname, '..', '..', 'index.html'));
-});
-
-console.log("Server started: " + PORT);
-var server = app.listen(PORT);
+console.log("Server started: " + PORT)
+var server = app.listen(PORT)
